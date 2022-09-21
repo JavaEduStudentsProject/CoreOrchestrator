266b62f778c26082c3157606159b35555f5792ce
IrinaPostnikova <iri.postnikova@gmail.com>
2022-09-21 14:17:09 +0300

    connection orchestra to db

---
+++ src/main/java/com/example/orchestrator/controller/FrontController.java
@@ -0,0 +1,71 @@
+package com.example.orchestrator.controller;
+
+import com.example.orchestrator.kafka.MessageProducer;
+import com.example.orchestrator.kafka.MessageProducerFile;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+public class FrontController {
+    MessageProducer messageProducer;
+    MessageProducerFile messageProducerFile;
+
+    @Autowired
+    public FrontController(MessageProducer messageProducer, MessageProducerFile messageProducerFile) {
+        this.messageProducer = messageProducer;
+        this.messageProducerFile = messageProducerFile;
+    }
+    @GetMapping()
+    @KafkaListener(topics = "parseFile", containerFactory = "kafkaListenerContainerFactory")
+    public void listenerParseFile() {
+        File file = new File("CoreOrchestrator/file.csv");
+        log.info("Listener orchestrator: file from Front {}", file.getName());
+        messageProducerFile.sendMessage(file, "parseFile");
+        log.info("Producer orchestrator: file {} to Parser, topicFrontToParser", file.getName());
+    }
+    @GetMapping("/get")
+    // Гет продукт от фронта в базу//
+    @KafkaListener(topics = "getProduct", containerFactory = "kafkaListenerContainerFactory")
+    public void listenerGetProduct(String id) {
+               id="1";
+        log.info("Get request from Front 'get product'");
+        messageProducer.sendMessage(id, "getProduct");// направляем запрос в базу
+        log.info("Redirect request to Database 'get product' with id = {}", id);
+    }
+    //этого метода пока нет в бд
+//    @KafkaListener(topics = "getAllProducts", containerFactory = "kafkaListenerContainerFactory")
+//    public void listenerGetAllProducts(String products) {
+//        log.info("Get request from Front 'get all products'");
+//        messageProducer.sendMessage(products, "getAllProducts");// направляем запрос в базу
+//        log.info("Redirect request to Database 'get product' with id = {}", products);
+//    }
+    @KafkaListener(topics = "saveProduct", containerFactory = "kafkaListenerContainerFactory")
+    public void listenerSaveProduct(String product) {
+        log.info("Get request from Front 'save product'");
+        messageProducer.sendMessage(product, "saveProduct");// направляем запрос в базу
+        log.info("Redirect request to Database 'save product' with id = {}", product);
+    }
+
+    @KafkaListener(topics = "deleteProduct", containerFactory = "kafkaListenerContainerFactory")
+    public void listenerDeleteProduct(String id) {
+        log.info("Get request from Front 'delete product'");
+        messageProducer.sendMessage(id, "deleteProduct");// направляем запрос в базу
+        log.info("Redirect request to Database 'delete product' with id = {}", id);
+    }
+
+    @KafkaListener(topics = "updateProduct", containerFactory = "kafkaListenerContainerFactory")
+    public void listenerUpdateProduct(String id) {
+        log.info("Get request from Front 'update product'");
+        messageProducer.sendMessage(id, "updateProduct");// направляем запрос в базу
+        log.info("Redirect request to Database 'update product' with id = {}", id);
+    }
+
+}
--- src/main/java/com/example/orchestrator/kafka/KafkaTopicConfig.java
+++ src/main/java/com/example/orchestrator/kafka/KafkaTopicConfig.java
@@ -18,13 +18,68 @@ public class KafkaTopicConfig {
     @Value(value = "${kafka.bootstrapAddress}")
     private String bootstrapAddress;
 
-
     @Bean
     public KafkaAdmin kafkaAdmin() {
         Map<String, Object> config = new HashMap<>();
         config.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-
         return new KafkaAdmin(config);
+    }
+
+    @Bean
+    public NewTopic parseFile() {
+        log.info("Create orchestrator topic parseFile");
+        return new NewTopic("parseFile", 1, (short) 1);
+    }
+
+    @Bean
+    public NewTopic getProduct() {
+        log.info("Create orchestrator topic getProduct");
+        return new NewTopic("getProduct", 1, (short) 1);
+    }
+
+    @Bean
+    public NewTopic getAllProducts() {
+        log.info("Create orchestrator topic getAllProducts");
+        return new NewTopic("getAllProducts", 1, (short) 1);
+    }
+
+    @Bean
+    public NewTopic saveProduct() {
+        log.info("Create orchestrator topic saveProduct");
+        return new NewTopic("saveProduct", 1, (short) 1);
+    }
+    @Bean
+    public NewTopic save() {
+        log.info("Create orchestrator topic save");
+        return new NewTopic("save", 1, (short) 1);
+    }
+    @Bean
+    public NewTopic deleteProduct() {
+        log.info("Create orchestrator topic deleteProduct");
+        return new NewTopic("deleteProduct", 1, (short) 1);
+    }
+
+    @Bean
+    public NewTopic updateProduct() {
+        log.info("Create orchestrator topic updateProduct");
+        return new NewTopic("updateProduct", 1, (short) 1);
+    }
+
+    @Bean
+    public NewTopic sendProduct() {
+        log.info("Create orchestrator topic sendProduct");
+        return new NewTopic("sendProduct", 1, (short) 1);
+    }
+
+    @Bean
+    public NewTopic sendAllProduct() {
+        log.info("Create orchestrator topic sendAllProduct");
+        return new NewTopic("sendAllProduct", 1, (short) 1);
+    }
+
+    @Bean
+    public NewTopic topicReact() {
+        return new NewTopic("test-topic", 2, (short) 1);
     }
 
     @Bean
@@ -32,51 +87,4 @@ public class KafkaTopicConfig {
         log.info("Create orchestrator topic parser");
         return new NewTopic("parser", 1, (short) 1);
     }
-
-    @Bean
-    public NewTopic topicFrontToParser() {
-        log.info("Create orchestrator topic topicFrontToParser");
-        return new NewTopic("topicFrontToParser", 1, (short) 1);
-    }
-    @Bean
-    public NewTopic topicSave() {
-        log.info("Create orchestrator topic save");
-        return new NewTopic("save", 2, (short) 1);
-    }
-
-    //    @Bean
-//    public NewTopic topicSaveOne() {
-//        return new NewTopic("saveOne", 2, (short) 1);
-//    }
-
-    @Bean
-    public NewTopic topicReact() {
-        return new NewTopic("test-topic", 2, (short) 1);
-    }
-    @Bean
-    public NewTopic topicUpdate() {
-        return new NewTopic("update", 2, (short) 1);
-    }
-
-    @Bean
-    public NewTopic topicDelete() {
-        return new NewTopic("delete", 2, (short) 1);
-    }
-
-    @Bean
-    public NewTopic topicGetOne() {
-        return new NewTopic("getOne", 2, (short) 1);
-    }
-
-    @Bean
-    public NewTopic topicGetAll() {
-        return new NewTopic("getAll", 2, (short) 1);
-    }
-    //topicName для создания новых топик
-
-//    @Bean
-//    public NewTopic topic() {
-//        return new NewTopic(topicName, 1, (short) 1);
-//    }
-
 }
