54c326c0f874ada8696c64a0728f0b5f9f6b62c1
Midya <midyaso5439@gmail.com>
2022-09-22 22:47:22 +0300

    change log

--- src/main/java/com/example/orchestrator/model/JsonHamsterItem.java
+++ src/main/java/com/example/orchestrator/model/JsonHamsterItem.java
@@ -3,8 +3,6 @@ package com.example.orchestrator.model;
 import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Getter
