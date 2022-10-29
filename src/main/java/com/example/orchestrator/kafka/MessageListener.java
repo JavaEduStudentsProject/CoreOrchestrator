1ebcc06ae71e79efd07c1d6711fe231bba88d225
IrinaPostnikova <iri.postnikova@gmail.com>
2022-10-29 18:34:32 +0300

    review

--- src/main/java/com/example/orchestrator/kafka/MessageListener.java
+++ src/main/java/com/example/orchestrator/kafka/MessageListener.java
@@ -23,37 +23,41 @@ public class MessageListener {
     @Autowired
     KafkaConsumerConfig config;
     private final String topicProducts = "sendALlProducts";
-   private final String sendHamster = "SendHamster";
+    private final String sendHamster = "SendHamster";
+
+    private final String sendOrdersDataFromDB = "sendOrdersDataFromDB";
+
     public MessageListener() {
     }
 
     public String listenerGetAllProductsResponse() {
         log.info("Sent request to Database - get products");
         messageProducer.sendMessage("get all products", "getAllProductsDB");
-        String products= null;
+
+
+        String products = null;
         Consumer<String, String> consumer = (Consumer<String, String>) factoryString.getConsumerFactory().createConsumer();
         consumer.subscribe(Collections.singleton(topicProducts));
 
+
         ConsumerRecords<String, String> productsRecords = consumer.poll(10000);
-        for (ConsumerRecord<String,String> record : productsRecords)
-        {
+        for (ConsumerRecord<String, String> record : productsRecords) {
             products = record.value();
         }
         consumer.close();
         return products;
     }
 
- //   для получения ордеров из бд
+    //   для получения ордеров из бд
     public String listenerGetAllOrdersResponse() {
         log.info("Sent request to Database - get orders");
-        messageProducer.sendMessage("get all orders", "GetAllOrders");
+       messageProducer.sendMessage("get all orders", "GetAllOrders");
+
         String orders = null;
         Consumer<String, String> consumer = (Consumer<String, String>) factoryString.getConsumerFactory().createConsumer();
         consumer.subscribe(Collections.singleton(sendHamster));
-
-        ConsumerRecords<String, String> ordersRecords = consumer.poll(10000);
-        for (ConsumerRecord<String,String> record : ordersRecords)
-        {
+                ConsumerRecords<String, String> ordersRecords = consumer.poll(10000);
+        for (ConsumerRecord<String, String> record : ordersRecords) {
             orders = record.value();
         }
         consumer.close();
